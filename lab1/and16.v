ca35625170785fe83c57a5a13c991da1231ca2c4
michaelkarapetyan9 <149108783+michaelkarapetyan9@users.noreply.github.com>
2026-04-06 10:56:39 -0700

    Create and16.v

---
+++ lab1/and16.v
@@ -0,0 +1,12 @@
+module and16(
+  output [15:0] y,
+  input  [15:0] a,
+  input  [15:0] b
+);
+  genvar i;
+  generate
+    for (i = 0; i < 16; i = i + 1) begin : ANDBITS
+      and (y[i], a[i], b[i]);
+    end
+  endgenerate
+endmodule
