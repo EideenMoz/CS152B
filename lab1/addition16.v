60a9990dc601ad26df420fedcde2b931e7b6ab7f
Michael Karapetyan <michaelkarapetyan04@gmail.com>
2026-04-06 11:55:31 -0700

    First lab day: or and initial additon logic

---
+++ lab1/addition16.v
@@ -0,0 +1,21 @@
+module addition16(
+    output [15:0] sum,
+    output        cout,
+    output        overflow,
+    input  [15:0] a,
+    input  [15:0] b,
+    input         cin
+);
+    wire [16:0] c;
+    assign c[0] = cin;
+
+    genvar i;
+    generate
+        for (i = 0; i < 16; i = i + 1) begin : ADDSTAGE
+            full_adder fa(sum[i], c[i+1], a[i], b[i], c[i]);
+        end
+    endgenerate
+
+    assign cout = c[16];
+    xor (overflow, c[15], c[16]);
+endmodule
