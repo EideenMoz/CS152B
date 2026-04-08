78309a93d4b17d0104080ae2b0dc38fae4f218e7
Eideen Mozaffari <eideenm11@gmail.com>
2026-04-08 12:35:10 -0700

    Add 8-to-1 multiplexer module for 16-bit inputs

---
+++ lab1/mux8_1_16bit.v
@@ -0,0 +1,14 @@
+module mux8_1_16bit (
+  output wire [15:0] out, 
+  input wire [15:0] in0, in1, in2, in3, in4, in5, in6, in7,
+  inout wire [2:0] sel
+);
+  wire [15:0] low_four, high_four;
+
+  // First stage: Two 4:1 Muxes using lower bits sel[1:0]
+  mux4_1_16bit(low_four, in0, in1, in2, in3, sel[1:0]);
+  mux4_1_16bit(high_four, in4, in5, in6, in7, sel[1:0]);
+
+  // Second stage: One 2:1 Mux using MSB sel[2]
+  mux2_1_16bit(out, low_four, high_four, sel[2]);
+endmodule
