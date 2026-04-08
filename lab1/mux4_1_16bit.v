0942c01cc0b098e9d5a30101e141643fe9733154
Eideen Mozaffari <eideenm11@gmail.com>
2026-04-08 12:31:36 -0700

    Rename wires for clarity in mux4_1_16bit

--- lab1/mux4_1_16bit.v
+++ lab1/mux4_1_16bit.v
@@ -3,14 +3,14 @@ module mux4_1_16bit (
   input wire [15:0] in0, in1, in2, in3,
   input wire [1:0] sel
 );
-  wire [15:0] mux_low, mux_high;
+  wire [15:0] low_two, high_two;
 
   // First stage: Select one element from each of the sets (in0,in1), (in2,in3)
-  mux2_1_16bit(mux_low, in0, in1, sel[0]);     // Check LSB of I0 and I1
-  mux2_1_16bit(mux_high, in2, in3, sel[0]);    // Check LSB of I2 and I3
+  mux2_1_16bit(low_two, in0, in1, sel[0]);     // Check LSB of I0 and I1
+  mux2_1_16bit(high_two, in2, in3, sel[0]);    // Check LSB of I2 and I3
 
   // Second stage: Look at MSB
-  mux2_1_16bit(out, mux_low, mux_high, sel[1]);
+  mux2_1_16bit(out, low_two, high_two, sel[1]);
 endmodule
   
   
