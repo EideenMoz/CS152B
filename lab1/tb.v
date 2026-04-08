a605d6319b5d60586439e09e6e3bd5090aea5718
Pedro Rodrigues Langone <149747093+Pedro-Langone@users.noreply.github.com>
2026-04-08 11:42:25 -0700

    OR/AND tb. created top module and zero flag

--- lab1/tb.v
+++ lab1/tb.v
@@ -1,29 +1,31 @@
 `timescale 1ns / 1ps
 module tb();
 
-wire [15:0] result_S;
+wire [15:0] S;
 wire cout;
 wire overflow;
+wire zero;
 reg [15:0] a;
 reg [15:0] b;
-reg cin;
+reg [3:0] opcode;
 
-addition16 add (
-    .sum(result_S),
-    .cout(cout),
-    .overflow(overflow),
+ALU_top dut (
     .a(a),
     .b(b),
-    .cin(cin)
+    .ALU_ctrl(opcode), 
+    .result(S),
+    .zero(zero),
+    .overflow(overflow)
 );
 
 initial begin 
-    //INT_MAX=32767, INT_MIN=-32,768
+    //INT_MAX=32767, INT_MIN =-32,768
 
-    //===TEST addition16===//
+    //===TEST addition16===\\
+    opcode=1;
 
     //Regular sums
-    a=16'h0000; b=16'h0000; cin=0; #10;
+    a=16'h0000; b=16'h0000; #10;
     a=16'h0001; b=16'h0000; #10;
     a=16'h0000; b=16'h0001; #10;
     a=16'h0001; b=16'h0001; #10;
@@ -37,7 +39,7 @@ initial begin
     a=32766; b=1; #10;
 
     // Negative smalltests
-    a = -1; b = -1; cin = 0; #10;
+    a = -1; b = -1; #10;
     a = -5; b = 3; #10;
     a=-15000; b=-10111; #10;
 
@@ -49,13 +51,38 @@ initial begin
     // Carry vs overflow
     a = 16'hFFFF; b = 1; #10;
 
-    // CIN tests
-    a = 0; b = 0; cin = 1; #10;
-    a = 32767; b = 0; cin = 1; #10;
-
     // Boundary
     a = -32768; b = 1; #10;
     a = 32767; b = -1; #10;
+
+    //===TEST bitwise OR===\\
+    opcode=2;
+
+    // Regular OR
+    a=16'h0000; b=16'h0000; #10;
+    a=16'hFFFF; b=16'h0000; #10;
+    a=16'h0000; b=16'hFFFF; #10;
+    a=16'hAAAA; b=16'h5555; #10; // alternating bits
+    a=16'h8000; b=16'h0001; #10; // MSB and LSB
+
+    // Edge OR cases
+    a=16'hFFFF; b=16'hFFFF; #10;
+    a=16'h7FFF; b=16'h8000; #10; // boundary
+    a=16'h0001; b=16'h8000; #10;
+
+    //===TEST bitwise AND===\\
+    opcode=3;
+
+    // Regular AND
+    a=16'hFFFF; b=16'h0000; #10;
+    a=16'hFFFF; b=16'hFFFF; #10;
+    a=16'hAAAA; b=16'h5555; #10; // alternating bits
+    a=16'h8000; b=16'h0001; #10; // MSB and LSB
+
+    // Edge AND cases
+    a=16'h7FFF; b=16'h8000; #10; // boundary
+    a=16'h0001; b=16'h8000; #10;
+    a=16'h0000; b=16'h0000; #10;
 
 
     $finish;
