6bc92f524fa6bda30068bbe41a72e43e9c98ba8e
Pedro Langone <pedrolangone@yahoo.com.br>
2026-04-10 13:42:32 -0700

    Added muxes for result and overflow to top

--- lab1/ALU_top.v
+++ lab1/ALU_top.v
@@ -7,14 +7,6 @@ module ALU_top(
     output wire zero,
     output wire overflow
 );
-    //temp for tb to work
-    reg [15:0] result_reg;
-    assign result = result_reg;
-    reg subtract;
-    initial subtract=0;
-    reg overflow_reg;
-    assign overflow = overflow_reg;
-
     //===Addition===\\
     wire [15:0] result_sum;
     wire [15:0] overflow_sum;
@@ -99,48 +91,88 @@ module ALU_top(
         .zero(zero)
     );
 
-    always @(*) begin
-        // Default values
+    mux16_1_16bit mux (
+        .out(result),
+        .in0(result_sub),
+        .in1(result_sum),
+        .in2(result_or),
+        .in3(result_and),
+        .in4(result_dec),  
+        .in5(result_inc),  
+        .in6(result_inv),
+        .in7(0),      
+        .in8(0), 
+        .in9(0),       
+        .in10(0),      
+        .in11(0), 
+        .in12(0),   
+        .in13(0),  
+        .in14(0), 
+        .in15(0),  
+        .sel(ALU_ctrl)
+    );
+    mux16_1_16bit overflow_mux (
+        .out(overflow),
+        .in0(overflow_sub),
+        .in1(overflow_sum),
+        .in2(0), //no overflow for bitwise OR
+        .in3(0), //no overflow for bitwise AND
+        .in4(overflow_dec),  
+        .in5(overflow_inc),  
+        .in6(overflow_inv),
+        .in7(0),      
+        .in8(0), 
+        .in9(0),       
+        .in10(0),      
+        .in11(0), 
+        .in12(0),   
+        .in13(0),  
+        .in14(0), 
+        .in15(0),  
+        .sel(ALU_ctrl)
+    );
+    // always @(*) begin
+    //     // Default values
 
-        case(ALU_ctrl)
-            4'h0: begin 
-                result_reg = result_sub;    //sub
-                overflow_reg = overflow_sub;
-            end
-            4'h1: begin
-                result_reg = result_sum;    //add
-                overflow_reg = overflow_sum;
-            end
-            4'h2: begin
-                result_reg = result_or; // OR
-                overflow_reg = 1'b0;
-            end
-            4'h3: begin
-                result_reg = result_and;  // AND
-                overflow_reg = 1'b0;
-            end
-            4'h4: begin
-                result_reg = result_dec;      // decrement
-                overflow_reg = overflow_dec;
-            end
-            4'h5: begin
-                result_reg = result_inc;         // increment
-                overflow_reg = overflow_inc;
-            end
-            4'h6: begin
-                result_reg = result_inv;     // invert
-                overflow_reg = overflow_inv; 
-            end
-            4'h7: result_reg = a << 1; // Shift left logical
-            4'h8: result_reg = $signed(a) >>> 1; // Shift right arithmetic
-            4'h9: result_reg = a + 1;      // Increment
-            4'hA: result_reg = a - 1;      // Decrement
-            4'hB: result_reg = {a[7:0], b[7:0]}; // Example concatenation
-            4'hC: result_reg = a & ~b;     // AND-NOT
-            4'hD: result_reg = a | ~b;     // OR-NOT
-            4'hE: result_reg = ~(a | b);   // NOR
-            4'hF: result_reg = ~(a & b);   // NAND
-            default: result_reg = 16'h0000;
-        endcase
-    end
+    //     case(ALU_ctrl)
+    //         4'h0: begin 
+    //             result_reg = result_sub;    //sub
+    //             overflow_reg = overflow_sub;
+    //         end
+    //         4'h1: begin
+    //             result_reg = result_sum;    //add
+    //             overflow_reg = overflow_sum;
+    //         end
+    //         4'h2: begin
+    //             result_reg = result_or; // OR
+    //             overflow_reg = 1'b0;
+    //         end
+    //         4'h3: begin
+    //             result_reg = result_and;  // AND
+    //             overflow_reg = 1'b0;
+    //         end
+    //         4'h4: begin
+    //             result_reg = result_dec;      // decrement
+    //             overflow_reg = overflow_dec;
+    //         end
+    //         4'h5: begin
+    //             result_reg = result_inc;         // increment
+    //             overflow_reg = overflow_inc;
+    //         end
+    //         4'h6: begin
+    //             result_reg = result_inv;     // invert
+    //             overflow_reg = overflow_inv; 
+    //         end
+    //         4'h7: result_reg = a << 1; // Shift left logical
+    //         4'h8: result_reg = $signed(a) >>> 1; // Shift right arithmetic
+    //         4'h9: result_reg = a + 1;      // Increment
+    //         4'hA: result_reg = a - 1;      // Decrement
+    //         4'hB: result_reg = {a[7:0], b[7:0]}; // Example concatenation
+    //         4'hC: result_reg = a & ~b;     // AND-NOT
+    //         4'hD: result_reg = a | ~b;     // OR-NOT
+    //         4'hE: result_reg = ~(a | b);   // NOR
+    //         4'hF: result_reg = ~(a & b);   // NAND
+    //         default: result_reg = 16'h0000;
+    //     endcase
+    // end
 endmodule
