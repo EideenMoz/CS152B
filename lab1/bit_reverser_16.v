6acc75a89d3f79815e2db0d2a3e823ff93086a79
Eideen Mozaffari <eideenm11@gmail.com>
2026-04-10 14:23:34 -0700

    Add 16-bit bit reverser module
    Implement a 16-bit bit reverser module for use in ALU shifter unit.

---
+++ lab1/bit_reverser_16.v
@@ -0,0 +1,16 @@
+// Helper module for alu_shifter_unit. The barrel_core_16 only implements left shift.
+// To implement a right shift using only operations of left shift and reverse available, 
+// first apply the reverse operation, then apply the left shift operation, then apply the reverse operation again.
+module bit_reverser_16 (
+  output wire [15:0] out,
+  input wire [15:0] in,
+  input wire reverse    // Reverse "in" if reverse = 1 else do nothing
+);
+  genvar i;
+  generate
+    for (i=0; i<16; i=i+1) begin : REV_ARRAY
+      mux2_1 m (out[i], in[1], in[15-i], reverse);    // out[i]<--in[i] if reverse==0 else out[i]=in[15-i]
+    end
+  endgenerate
+endmodule
+      
