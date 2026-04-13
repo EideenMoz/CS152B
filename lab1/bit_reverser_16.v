601d4fe860db1bf658e44b00ea3d00a6dbb624c2
Pedro Langone <pedrolangone@yahoo.com.br>
2026-04-13 10:27:58 -0700

    Fixed typos

--- lab1/bit_reverser_16.v
+++ lab1/bit_reverser_16.v
@@ -1,6 +1,6 @@
-// Helper module for alu_shifter_unit. The barrel_core_16 only implements left shift.
-// To implement a right shift using only operations of left shift and reverse available, 
-// first apply the reverse operation, then apply the left shift operation, then apply the reverse operation again.
+// Helper module for alu_shifter_unit. The barrel_core_16 only implements right shift.
+// To implement a left shift using only operations of right shift and reverse available, 
+// first apply the reverse operation, then apply the right shift operation, then apply the reverse operation again.
 module bit_reverser_16 (
   output wire [15:0] out,
   input wire [15:0] in,
