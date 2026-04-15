d18b4b6e8e0f9cc3f58c4a91ccf6586f137d5d88
Eideen Mozaffari <eideenm11@gmail.com>
2026-04-15 10:46:13 -0700

    Refactor barrel shifter for improved clarity and functionality
    Refactor barrel shifter logic to simplify shift amount calculation and handle saturation.

--- lab1/alu_shifter_unit.v
+++ lab1/alu_shifter_unit.v
@@ -25,21 +25,21 @@ module alu_shifter_unit(
   not(should_reverse, is_right);
   bit_reverser_16 pre_rev (prepared_in, a, should_reverse);
 
+  // Barrel Shifter Core (handles 0-15)
+  // We pass b[3:0] directly to the core
+  barrel_core_16 core (shifter_out_raw, prepared_in, b[3:0], fill_bit);
+
   // Perform right shift
   // if b > d'15:
   //    shift_amount=16
   // else: shift_amount=b[3:0]
   wire b_gt_15; // Structural OR reduction of b[15:4]
   or g_or_upper (b_gt_15, b[4], b[5], b[6], b[7], b[8], b[9], b[10], b[11], b[12], b[13], b[14], b[15]);
-  wire [3:0] final_shift_amt;
-  // For each bit of the shift amount:
-  // If b_gt_15 is 1, output is 1 (from 4'b1111).
-  // If b_gt_15 is 0, output is b[i].
-  mux2_1 m0 (final_shift_amt[0], b[0], 1'b1, b_gt_15);
-  mux2_1 m1 (final_shift_amt[1], b[1], 1'b1, b_gt_15);
-  mux2_1 m2 (final_shift_amt[2], b[2], 1'b1, b_gt_15);
-  mux2_1 m3 (final_shift_amt[3], b[3], 1'b1, b_gt_15);
-  barrel_core_16 core (shifted_raw, prepared_in, final_shift_amt, fill_bit);
+
+ // Create a vector of all fill bits (e.g., all 0s or all 1s)
+  assign all_fill = {16{fill_bit}};
+  // If b_gt_15 is true, result is all_fill; else it is the core output
+  mux2_1_16bit saturation_mux (shifted_saturated, shifter_out_raw, all_fill, b_gt_15);
 
   // Post-shift-reverse (handle case when opcode specifies left shift)
   bit_reverser_16 post_rev (out, shifted_raw, should_reverse);
