6ae7846ab270badc098e39ff0636bfa659f79ba8
Pedro Langone <pedrolangone@yahoo.com.br>
2026-04-20 10:50:42 -0700

    Initial structure of tx

--- Lab2/tx.v
+++ Lab2/tx.v
@@ -1,6 +1,7 @@
 module tx (
     input clk,
     input rst,
+    input send,     //not on spec, but ig we need some way to leave idle state
     input [7:0] data,
     output reg tx_line
 );
@@ -8,16 +9,31 @@ module tx (
 # define IDLE 0
 # define SEND_DATA 1
 reg state;
+reg next_state;
+reg [3:0] position;
 
+//next state logic
+always (*) begin
+    case (state) 
+        IDLE: begin
+            
+        end
+        SEND_DATA: begin
+
+        end
+    endcase
+
+end
+
+//logic for each state
 always @(posedge clk or posedge rst) begin
     if (rst) begin
-        state <= IDLE;
+        position <= 0;
     end 
     else begin 
         case (state)
             IDLE: begin
                 tx_line <= 0; // START bit
-                state <= SEND_DATA;
             end
             SEND_DATA: begin
                 ;
@@ -26,3 +42,11 @@ always @(posedge clk or posedge rst) begin
     end
 end
 endmodule
+
+//update state
+always @(posedge clk or posedge rst) begin
+    if (rst)
+        state <= IDLE;
+    else
+        state <= next_state;
+end
