309e87faf2fd1ad92703c134adc09cda5ec56d48
Pedro Langone <pedrolangone@yahoo.com.br>
2026-04-20 10:11:43 -0700

    Added first lab2 file skeleton

---
+++ Lab2/rx.v
@@ -0,0 +1,8 @@
+module tx (
+    input clk,
+    input rst,
+    input [7:0] data,
+    output reg tx_line
+);
+
+endmodule
---
+++ Lab2/tx.v
@@ -0,0 +1,8 @@
+module tx (
+    input clk,
+    input rst,
+    input rx_line,
+    output reg [7:0] data
+);
+
+endmodule
